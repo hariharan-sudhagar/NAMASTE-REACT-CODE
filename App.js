604a5b55dbd56009ec3b5237384e398456f10392
hariharan-sudhagar <iamhariharan181@gmail.com>
2025-02-09 01:38:04 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,37 @@
+//    const heading= React.createElement(
+//     "h1",{id: "heading"},
+//     "Hello world form react!");
+
+//   const root = ReactDOM.createRoot(document.getElementById("root"));
+//    root.render(heading);
+//    console.log(heading);
+
+// /*
+//   <div id = "parent"> 
+//          <div id = "child"> 
+//             <h1>hello from h1</h1>
+//             <h2>hello from h1</h2>
+
+//   </div>
+// */
+
+//   const parent = React.createElement("div",{id: "parent"},
+//       [
+//       React.createElement("div",{id: "child"}),
+//     [ React.createElement("h1",{id: "h1"},"hello from h1"),
+//       React.createElement("h2",{id: "h2"},"hello from h2")
+//     ] , 
+//       React.createElement("div",{id: "child"}),
+//     [ React.createElement("h1",{id: "h1.1"},"hello from h1"),
+//       React.createElement("h2",{id: "h2.2"},"hello from h2")
+//     ]
+//       ] );  
+
+//       //JSX 
+//       const root1 = ReactDOM.createRoot(document.getElementById("root1"));
+
+//   root1.render(parent)
+//    // react is uses to manupilate dom by js
+
+   let hari =18;
+   console.log(size(hari))
